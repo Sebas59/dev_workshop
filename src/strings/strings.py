d48d0d680b81b7d3d6073cd5437529563cf768fa
Sebastian lopez <juancholour@gmail.com>
2025-03-05 16:42:36 -0500

    Se agrego la funcion palabras_mayus para el test de string

--- src/strings/strings.py
+++ src/strings/strings.py
@@ -20,16 +20,7 @@ class Strings:
         return len(cadena.split())
     
     def palabras_mayus(self, texto):
-        """
-        Pon en Mayuscula la primera letra de cada palabra en una cadena.
-        
-        Args:
-            texto (str): Cadena
-            
-        Returns:
-            str: Cadena con la primera letra de cada palabra en mayúscula
-        """
-        pass
+        return texto.title()
     
     def eliminar_espacios_duplicados(self, texto):
         """
